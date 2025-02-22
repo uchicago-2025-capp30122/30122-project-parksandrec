c1f1d0a4cc24dd08bc8765ee23b3588b172b58db
Chema Gálvez <chemagalvez@Chemas-MacBook-Air.local>
2025-02-22 16:06:39 -0600

    call function

--- parksandrec/preprocessing/geospatial.py
+++ parksandrec/preprocessing/geospatial.py
@@ -2,41 +2,36 @@ import geopandas as gpd
 from shapely import Polygon
 import pickle
 
-# Replace with path shapefile
-shapefile_path = ''
+def merge_datasets(landuse_path, tract_path):
+    # Load data
+    land_use_shapefile_path = landuse_path
+    tract_shapefile_path = tract_path
 
-# Read the shapefile into a GeoDataFrame
-gdf_landuse = gpd.read_file(shapefile_path)
+    # Load as geopandas
+    gdf_landuse = gpd.read_file(land_use_shapefile_path)
+    gdf = gpd.read_file(tract_shapefile_path)
 
-# Display the first few rows of the data
-gdf_landuse.info()
+    # Change landuse coordinates
+    gdf_landuse = gdf_landuse.to_crs("EPSG:4269")
 
-# Replace with path shapefile
-shapefile_path = '/Users/chemagalvez/capp/projects/data/cb_2018_17_tract_500k/cb_2018_17_tract_500k.shp'
+    gdf_landuse['centroid'] = gdf_landuse['geometry'].centroid
 
-# Read the shapefile into a GeoDataFrame
-gdf = gpd.read_file(shapefile_path)
+    # Add dummy column before merge
+    gdf_landuse['census_tract'] = 0
 
-# Display the first few rows of the data
-gdf.info()
+    # Filter only for cook_county
+    cook_county_land = gdf_landuse[gdf_landuse['FIRST_COUN'] == '031'].head(10)
+    cook_county_tracts = gdf[gdf['COUNTYFP'] == '031']
 
-# Change landuse coordinates
-gdf_landuse = gdf_landuse.to_crs("EPSG:4269")
+    # Merge the datasets
+    count = 0
+    for _, land_parsel in cook_county_land.iterrows():
+        for __, tract in cook_county_tracts.iterrows():
+            if tract['geometry'].contains(land_parsel['centroid']):
+                cook_county_land['census_tract'] = cook_county_tracts['TRACTCE']
+                break
 
-gdf_landuse['centroid'] = gdf_landuse['geometry'].centroid
+    cook_county_land.to_pickle('../../../merged_df.pkl')
 
-# Add dummy column before merge
-gdf_landuse['census_tract'] = 0
-
-# Filter only for cook_county
-cook_county_land = gdf_landuse[gdf_landuse['FIRST_COUN'] == '031']
-cook_county_tracts = gdf[gdf['COUNTYFP'] == '031']
-
-# Merge the datasets
-count = 0
-for index, land_parsel in cook_county_land.iterrows():
-    for jindex, tract in cook_county_tracts.iterrows():
-        if tract['geometry'].contains(land_parsel['centroid']):
-            count += 1
-            break
-
+merge_datasets('/Users/chemagalvez/capp/projects/data/2018_Land_Use_Inventory_for_Northeastern_Illinois/2018_Land_Use_Inventory_for_Northeastern_Illinois.shp', 
+               '/Users/chemagalvez/capp/projects/data/cb_2018_17_tract_500k/cb_2018_17_tract_500k.shp')
