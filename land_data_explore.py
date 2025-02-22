867c20b61e12a236b98b028aba8f34f926ea03fa
Raghav Mehrotra <raghavmehrotra@Raghavs-MacBook-Air.local>
2025-02-22 16:18:17 -0600

    Added mock visualization

---
+++ land_data_explore.py
@@ -0,0 +1,9 @@
+import json
+import geopandas
+
+#f_2005 = open("../data/land_use_2005.geojson")
+#land_2005 = json.load(f_2005)
+f_2018 = open("../data/land_use_2018.geojson")
+land_2018 = json.load(f_2018)
+
+print(land_2018)
