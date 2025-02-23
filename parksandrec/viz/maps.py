468c1368739b54ac1b4604d5fa78d071c5ecfd47
Raghav Mehrotra <raghavmehrotra@Raghavs-MacBook-Air.local>
2025-02-23 13:44:59 -0600

    Added new visualizatio

--- parksandrec/viz/maps.py
+++ parksandrec/viz/maps.py
@@ -2,7 +2,7 @@ import altair as alt
 from vega_datasets import data
 import pygris
 
-def plot_park_density():
+def plot_open_space_density():
     # 1. Cloropleth map at the county level: https://altair-viz.github.io/gallery/choropleth.html
     # They do not have any census tract data, will need to check how to analyze 
     # at that geospatial scale.
@@ -22,3 +22,40 @@ def plot_park_density():
         width=500,
         height=300
     )
+
+def plot_open_space():
+    """
+    From https://altair-viz.github.io/gallery/point_map.html
+    Plot the location of all open space in Chicago on a map.
+    """
+
+    # airports will be replaced with a dataframe containing the census
+    # tract ID and the total population of the tract for each open space parcel
+    airports = data.airports()
+
+    # Polygons for open spaces will be passed in through the merged dataframe
+    # from merge.py
+    states = alt.topo_feature(data.us_10m.url, feature='states')
+
+    # The background will be changed to include only Illinois, and zoomed into
+    # Cook County
+    background = alt.Chart(states).mark_geoshape(
+        fill='lightgray',
+        stroke='white'
+    ).properties(
+        width=500,
+        height=300
+    ).project('albersUsa')
+
+    # Open space positions on background
+    points = alt.Chart(airports).mark_circle(
+        size=10,
+        color='steelblue'
+    ).encode(
+        longitude='longitude:Q',
+        latitude='latitude:Q',
+        # This will become ['tract', 'area', 'total_pop']
+        tooltip=['name', 'city', 'state']
+    )
+
+    background + points
