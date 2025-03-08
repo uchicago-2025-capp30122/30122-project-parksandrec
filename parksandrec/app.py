facbe3d9a90dd2424b56467a0d106b17dbf1c8f3
Raghav Mehrotra <raghavmehrotra@Raghavs-MacBook-Air.local>
2025-03-08 16:50:47 -0600

    Structure for Dash app

--- parksandrec/app.py
+++ parksandrec/app.py
@@ -1,12 +1,12 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import Dash, dcc, html, Input, Output, callback
 import plotly.graph_objects as go
 import json
 import pandas as pd
 import pygris
 
-app = dash.Dash(__name__)
+from parksandrec.viz import charts, maps
+
+app = Dash()
 
 il_tracts = pygris.tracts(state = "IL", county = "031", year = 2018, cb = True)
 geojson_data = json.loads(il_tracts['geometry'].to_json())
@@ -35,17 +35,59 @@ fig.update_layout(
 
 )
 
-app.layout = html.Div([
-    html.H1("Choropleth Map of Illinois Tracts"),
-    dcc.Graph(
-        id='choropleth-map',
-        figure=fig,
-        style={
-            'height': '80vh', 
-            'width': '100%'    
-        }
-    )
-])
+default_graph = charts.plot_income_open_space('tot_open_space_prop', '<', 0.01)
+
+app.layout = [
+    html.Div([
+        html.H1("Choropleth Map of Illinois Tracts"),
+        dcc.Graph(
+            id='choropleth-map',
+            figure=fig,
+            style={
+                'height': '80vh', 
+                'width': '100%'    
+            }
+        )
+    ]),
+
+    html.Div([
+        html.Label("Enter open space type: "),
+        dcc.Dropdown(
+            id = 'open-space-dropdown',
+            options = {
+                'tot_open_space_prop': 'All',
+                '3100_prop': 'Recreational Space',
+                '3200_prop': 'Golf Courses',
+                '3300_prop': 'Conservation Areas',
+                '3400_prop': 'Private Open Space',
+                '4110_prop': 'Vacant Residential Land',
+                '4120_prop': 'Vacant Commercial Land',
+                '4130_prop': 'Vacant Industrial Land'},
+            value = 'tot_open_space_prop'
+        ),
+        html.Label("Enter inequality direction: "),
+        dcc.RadioItems(
+            id = 'direction-radio-button',
+            options = [
+                {'label': 'less than', 'value': '<'},
+                {'label': 'greater than', 'value': '>'}
+            ],
+            value = '<'
+        ),
+        html.Label("Threshold (percent): "),
+        dcc.Input(type='number', id='threshold-input', value=10),
+        dcc.Graph(figure=default_graph, id='income-graph')
+    ])
+]
+
+@callback(
+    Output(component_id='income-graph', component_property='figure'),
+    Input(component_id='open-space-dropdown', component_property='value'),
+    Input(component_id='direction-radio-button', component_property='value'),
+    Input(component_id='threshold-input', component_property='value'),
+)
+def update_income_graph(open_space_col, inequality_dir, threshold):
+    return charts.plot_income_open_space(open_space_col, inequality_dir, threshold)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
