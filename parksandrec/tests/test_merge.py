2d6e7e7f2c24a9d22695021339662e5e8d4f64f1
pabloher95 <phernandezpedraz@uchicago.edu>
2025-03-09 18:27:38 -0500

    drafting tests on merge

--- parksandrec/tests/test_merge.py
+++ parksandrec/tests/test_merge.py
@@ -1,9 +1,10 @@
 import pytest
 import geopandas as gpd
-from preprocessing.merge import collapse_tract
+from parksandrec.preprocessing.merge import collapse_tract
 
 
 def test_geodf():
     test_df = collapse_tract()
     print(type(test_df))
     assert isinstance(test_df, gpd.GeoDataFrame)
+
