c22ac8f40bca49b0b85600749a0caee1fe8826e2
pabloher95 <phernandezpedraz@uchicago.edu>
2025-03-08 19:48:34 -0600

    updated choropleth behavior on app.py

--- parksandrec/app.py
+++ parksandrec/app.py
@@ -1,25 +1,30 @@
 from dash import Dash, dcc, html, Input, Output, callback
 import plotly.graph_objects as go
 import json
-import pandas as pd
-import pygris
-
-from parksandrec.viz import charts, maps
+import geopandas as gpd
+from preprocessing.merge import collapse_tract
+from parksandrec.viz import charts
 
 app = Dash()
 
-il_tracts = pygris.tracts(state = "IL", county = "031", year = 2018, cb = True)
-geojson_data = json.loads(il_tracts['geometry'].to_json())
+data_tract = collapse_tract()
+
+tract_geo = gpd.GeoDataFrame(data_tract, geometry = 'geometry') # REMOVE
+tract_geo.set_index('TRACTCE', inplace = True)
+geojson_data = json.loads(tract_geo['geometry'].to_json())
+
+tt_cols = tract_geo[['tract','tot_pop', 'avg_hh_size','median_hh_income', 'med_val_own_occ']]
 
 fig = go.Figure(
     go.Choropleth(
         geojson=geojson_data,
-        locations=il_tracts.index,
-        z=il_tracts['ALAND'],
+        locations= tract_geo.index,
+        z= tract_geo['tot_open_space_prop'],
         colorscale="Viridis",
-        zmin=il_tracts['ALAND'].min(),
-        zmax=il_tracts['ALAND'].max(),
-        marker_line_width=0
+        zmin=tract_geo['tot_open_space_prop'].min(),
+        zmax=tract_geo['tot_open_space_prop'].max(),
+        marker_line_width=0,
+        customdata = tt_cols,
     )
 )
 
@@ -28,18 +33,28 @@ fig.update_layout(
     autosize=True,
     geo=dict(
         visible=False,
-        showsubunits=True,  
-        showcountries=False  
+        showsubunits=True,
+        showcountries=False
     ),
-    dragmode="zoom", 
+    dragmode="zoom",
 
+)
+fig.update_traces(
+    hovertemplate =
+    "ID: %{customdata[0]}<br>" +
+    "Population: %{customdata[1]:,.0f}<br>" + 
+    "Avg HH size: %{customdata[2]}<br>" +
+    "Median HH Income: %{customdata[3]:$,.0f}<br>" +
+    "Median Property Value: %{customdata[4]:$,.0f}<br>"
+    "Open Space: %{z:.2%}<br>"+
+    "<extra></extra>"
 )
 
 default_graph = charts.plot_income_open_space('tot_open_space_prop', '<', 0.01)
 
 app.layout = [
     html.Div([
-        html.H1("Choropleth Map of Illinois Tracts"),
+        html.H1("Distribution of Open Space in Cook County by Census Tract"),
         dcc.Graph(
             id='choropleth-map',
             figure=fig,
