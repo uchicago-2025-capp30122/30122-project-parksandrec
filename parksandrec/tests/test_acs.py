efce4bb5a68bf60cc48c8721b423be93e80127aa
hussains123 <87001248+hussains123@users.noreply.github.com>
2025-03-11 18:21:50 -0500

    added tests for acs.py

--- parksandrec/tests/test_acs.py
+++ parksandrec/tests/test_acs.py
@@ -1,51 +1,55 @@
-import pytest 
+import pytest
+import unittest
 import os
+import pandas as pd
 from parksandrec.preprocessing import acs
 
-CENUS_KEY = os.getenv("CENSUS_KEY")
+CENSUS_KEY = os.getenv("CENSUS_KEY")
 
 
-# def test_get_census_data():
-#     # """
-#     # Tests that only cook county ACS data being queried
-#     # """
+expected_vars = [
+        "owner_occ_units",
+        "renter_occ_units",
+        "med_val_own_occ",
+        "avg_hh_size",
+        "pop_25_ba",
+        "pop_25_hs",
+        "pop_disability",
+        "median_hh_income",
+        "tot_pop",
+        "white",
+        "black",
+        "native",
+        "asian",
+        "native_hawaiian",
+        "two_or_more_races",
+        "under_18",
+        "65_over",
+    ]
 
-#     # acs_df = acs.get_census_data(CENUS_KEY)
-    
-#     # assert acs_df[""] 
+def test_get_census_basic():
+    """
+    Tests for following:
+    - ACS Data is pandasDF
+    - Expected variables are in the ACS data
+    """
+ 
+    clean_acs = acs.acs_clean(CENSUS_KEY)
 
+    # ACS data is a pandas df
+    assert isinstance(clean_acs, pd.DataFrame)
 
+    # expected vars in df
+    for var in expected_vars:
+        assert var in clean_acs.columns
 
 
 def test_acs_clean():
     """
-    Tests for whether there are any negative values for relevant 
+    Tests for whether there are any negative values for relevant
     int variables
     """
 
-    clean_acs = acs.acs_clean(CENUS_KEY)
-    int_cols = [
-      "owner_occ_units",
-      "renter_occ_units",
-      "med_val_own_occ",
-      "avg_hh_size",
-      "pop_25_ba",
-      "pop_25_hs",
-      "pop_disability",
-      "median_hh_income",
-      "tot_pop",
-      "white",
-      "black",
-      "native",
-      "asian",
-      "native_hawaiian",
-      "two_or_more_races",
-      "under_18",
-      "65_over",
-      "18_64"
-      ]
-    
-    assert not (clean_acs[int_cols] < 0).any().any()
+    clean_acs = acs.acs_clean(CENSUS_KEY)
 
-
-
+    assert not (clean_acs[expected_vars] < 0).any().any()
