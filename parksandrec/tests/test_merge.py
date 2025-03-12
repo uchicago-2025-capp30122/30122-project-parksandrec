800ca4fa00ac77745069fc0bd5a0d24acacbc793
pabloher95 <phernandezpedraz@uchicago.edu>
2025-03-11 23:40:16 -0500

    fixed package environment bugs, updated tests to merge.py, and fixed bug in __main__.py

--- parksandrec/tests/test_merge.py
+++ parksandrec/tests/test_merge.py
@@ -2,6 +2,8 @@ import pytest
 import geopandas as gpd
 from parksandrec.preprocessing.merge import collapse_tract
 from shapely.geometry import Polygon
+from pyproj import CRS
+
 
 
 def test_uniqueness():
@@ -13,4 +15,8 @@ def test_geodf():
     assert isinstance(test_df, gpd.GeoDataFrame)
     for i, row in test_df.iterrows():
         assert isinstance(row['geometry'], Polygon)
+
+def test_crs():
+    test_df = collapse_tract()
+    assert test_df.crs == CRS("EPSG:4269")
 
